cfe79bb60909c148a7aae4358589001e83f92ba2
Kelvin9393 <kelvinlssiang9393@hotmail.com>
2018-01-16 12:52:25 +0800

    added tutorial files 01-06

---
+++ 01_pun/pun.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+    int i = 10;
+    float profit = 2150.43;
+    printf("To C, or not to C: that is the question.\n");
+    printf("%d\n", i);
+    printf("%.2f\n", profit);
+    return 0;
+}
